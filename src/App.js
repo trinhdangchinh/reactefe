a31318de7ad29a99e39f0d24fbf5aa339409e17e
henrytrinh <trinhdangchinh@gmail.com>
2022-09-27 08:24:46 +0700

    xóa logo svg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Chính
+        Chính 2
       </header>
     </div>
   );
